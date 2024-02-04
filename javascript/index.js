8d380d5eecd20fba1e7a06258408fd9c43688505
younessbennaj <youness.bennaj@gmail.com>
2024-02-04 17:13:55 +0100

    feat: add basic interactivity with JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+const triggers = document.querySelectorAll(".accordion-trigger");
+
+triggers.forEach((trigger) => {
+  trigger.addEventListener("click", (event) => {
+    const accordionItem = event.currentTarget.parentNode.parentNode;
+    accordionItem.classList.toggle("active");
+  });
+});
